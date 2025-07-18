ae2e8d1ff1cbdd5a46b265544bfb782dbf5b5e7c
Phoenixheman <himanshu.bhange@gmail.com>
2025-07-18 09:04:38 +0530

    Single Responsibility after

---
+++ single-responsibility-after.py
@@ -0,0 +1,45 @@
+class Order:
+
+    def __init__(self):
+        self.items = []
+        self.quantities = []
+        self.prices = []
+        self.status = "open"
+
+    def add_item(self, name, quantity, price):
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+
+    def total_price(self):
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.quantities[i] * self.prices[i]
+        return total
+
+
+
+class PaymentGateway:
+
+      def debitpay(self,order,security_code):
+            print("Processing debit payment type")
+            print(f"Verifying security code: {security_code}")
+            status = "paid"
+
+      def creditpay(self,order,security_code):
+            print("Processing credit payment type")
+            print(f"Verifying security code: {security_code}")
+            status = "paid"
+
+
+order = Order()
+order.add_item("Keyboard", 1, 50)
+order.add_item("SSD", 1, 150)
+order.add_item("USB cable", 2, 5)
+
+print(order.total_price())
+#PaymentGateway object with do debit payment and order does not contain paymenent method
+#result is same as before
+pay = PaymentGateway()
+pay.debitpay(order,"0372846")
+
